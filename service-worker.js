6bdef347214c5ce618d813af6c110d8393dce007
Muhammad Roisul Adib <99470918+roisuladib@users.noreply.github.com>
2023-03-15 15:59:11 +0700

    update isRipple, isStellar

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ee26633c2531a10f2c3747191fe6d62.js"
+  "/precache-manifest.e48dde9078b3d6fd092ab452a73216bb.js"
 );
 
 self.addEventListener('message', (event) => {
