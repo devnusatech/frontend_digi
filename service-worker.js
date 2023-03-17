4d988e6443730a84ec7172b9a3ed9e517d62ba60
Muhammad Roisul Adib <99470918+roisuladib@users.noreply.github.com>
2023-03-17 11:35:22 +0700

    transfer page

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.90f952ab5168fd9810fde512a3a4b0b1.js"
+  "/precache-manifest.53b7bab0dede1d3fa4a1685970211c32.js"
 );
 
 self.addEventListener('message', (event) => {
